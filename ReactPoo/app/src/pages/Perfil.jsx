2bd2593961edbc0e5b9967c2833a6f2d63952731
AnaGabriela-Dev <anagabirezende2006@gmail.com>
2025-11-28 15:08:39 -0300

    agora o usuário tem logout e parfil (supostamente) 2

--- ReactPoo/app/src/pages/Perfil.jsx
+++ ReactPoo/app/src/pages/Perfil.jsx
@@ -1,11 +1,21 @@
+import { useContext } from "react";
+import { UserContext } from "../context/UserContext";
 import { Header } from '../components/Header';
+
 import "../styles/perfil.css"
 
 export default function PerfilUsuario() {
+  const { user } = useContext(UserContext); // pega o user do contexto
+  
   return (
     <>
         <Header/>
-        <h1 className="title">Área do Usuário</h1>
+        <h1 className="title">
+          Olá, {user?.nome || "visitante"}!
+        </h1>
+        <div className="jogos-own">
+          <span className="jogos-user">Seus jogos:</span>
+        </div>
     </>
   )
 }
